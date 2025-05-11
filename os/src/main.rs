27ecc3f51e5343111b65cc277b55f0b9ab393dab
root <root@MSI>
2025-05-11 14:25:07 +0800

    5.11

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,33 +1,40 @@
-//! The main module and entrypoint
-//!
-//! The operating system and app also starts in this module. Kernel code starts
-//! executing from `entry.asm`, after which [`rust_main()`] is called to
-//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
-//! details.)
-//!
-//! We then call [`println!`] to display `Hello, world!`.
-
-#![deny(missing_docs)]
-#![deny(warnings)]
 #![no_std]
 #![no_main]
-#![feature(panic_info_message)]
 
-use core::arch::global_asm;
-use log::*;
-
-#[macro_use]
-mod console;
 mod lang_items;
-mod logging;
 mod sbi;
-
+mod console;
+//mod logging;
 #[path = "boards/qemu.rs"]
 mod board;
 
-global_asm!(include_str!("entry.asm"));
+const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
 
-/// clear BSS segment
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret;
+    unsafe {
+        core::arch::asm!(
+            "ecall",
+            inlateout("x10") args[0] => ret,
+            in("x11") args[1],
+            in("x12") args[2],
+            in("x17") id,
+        )
+    }
+    ret
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+core::arch::global_asm!(include_str!("entry.asm"));
+
 pub fn clear_bss() {
     extern "C" {
         fn sbss();
@@ -36,44 +43,11 @@ pub fn clear_bss() {
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 
-/// the rust entry-point of os
 #[no_mangle]
-pub fn rust_main() -> ! {
-    extern "C" {
-        fn stext(); // begin addr of text segment
-        fn etext(); // end addr of text segment
-        fn srodata(); // start addr of Read-Only data segment
-        fn erodata(); // end addr of Read-Only data ssegment
-        fn sdata(); // start addr of data segment
-        fn edata(); // end addr of data segment
-        fn sbss(); // start addr of BSS segment
-        fn ebss(); // end addr of BSS segment
-        fn boot_stack_lower_bound(); // stack lower bound
-        fn boot_stack_top(); // stack top
-    }
+pub fn rust_main() {
     clear_bss();
-    logging::init();
-    println!("[kernel] Hello, world!");
-    trace!(
-        "[kernel] .text [{:#x}, {:#x})",
-        stext as usize,
-        etext as usize
-    );
-    debug!(
-        "[kernel] .rodata [{:#x}, {:#x})",
-        srodata as usize, erodata as usize
-    );
-    info!(
-        "[kernel] .data [{:#x}, {:#x})",
-        sdata as usize, edata as usize
-    );
-    warn!(
-        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
-        boot_stack_top as usize, boot_stack_lower_bound as usize
-    );
-    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
 
+    println!("hello, world!");
     use crate::board::QEMUExit;
-    crate::board::QEMU_EXIT_HANDLE.exit_success(); // CI autotest success
-                                                   //crate::board::QEMU_EXIT_HANDLE.exit_failure(); // CI autotest failed
+    crate::board::QEMU_EXIT_HANDLE.exit_success(); 
 }
